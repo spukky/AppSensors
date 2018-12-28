6689fb874d3e1becf8f6934bb6136b91203bea80
Spukkii <darknessknight.darkness@gmail.com>
2018-12-28 14:07:41 +0700

    Edit Alert input not complete in AddPage

--- src/app/add-page/add-page.component.ts
+++ src/app/add-page/add-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { sensor } from '../sensor';
-import { ModalController } from '@ionic/angular';
+import { ModalController ,AlertController} from '@ionic/angular';
 import * as firebase from 'firebase/app';
 
 @Component({
@@ -17,50 +17,64 @@ export class AddPageComponent implements OnInit {
     timestamp: null,
     location:null
   };
+  currentColor :string = "#3E49BB" ;
   lat:number;
   lng:number;
-
-  constructor(public modalController: ModalController) { }
-
+  message:string;
+  constructor(public modalController: ModalController,public alertController: AlertController) { }
+  
   ngOnInit() {
-    // console.log("timestamp",this.s.timestamp);
-    // this.s.timestamp = new Date().toLocaleDateString(this.s.timestamp);
-    // console.log("timestamp",this.s.timestamp);
-    this.s.timestamp = new Date();
-
-    console.log("timestamp",this.s.timestamp);
-    this.s.timestamp = firebase.firestore.Timestamp.fromDate(this.s.timestamp);
-    console.log("timestamp",this.s.timestamp);
-    
-    
     
   }
-
+  
   save(){
-    console.log("timestamp",this.s.timestamp);
-    
-    this.s.timestamp = firebase.firestore.Timestamp.fromDate(new Date(this.s.timestamp) );
-    
-    console.log("time",this.s.timestamp);
-    console.log("this lat",this.lat);
-    
-    console.log("data",this.s);
-    this.s.location = new firebase.firestore.GeoPoint(this.lat,this.lng);    
-    this.modalController.dismiss(this.s);
-    // this.modalController.dismiss();
+    if((this.s.type == null )){
+      this.message = "type of sensor";
+      this.alertInput();
+    }
+    else if(this.s.name == null){
+      this.message = "name of sensor";
+      this.alertInput();
+    }
+    else if(this.s.value == null){
+      this.message = "value of sensor";
+      this.alertInput();
+    }
+    else if(this.lat == null || this.lng == null){
+      this.message = "location of sensor";
+      this.alertInput();
+    }
+    else if(this.s.timestamp == null){
+      this.s.timestamp = new Date();
+      this.s.timestamp = firebase.firestore.Timestamp.fromDate(this.s.timestamp);
+      this.modalController.dismiss(this.s);
+    }
+    else{
+      this.s.location = new firebase.firestore.GeoPoint(this.lat,this.lng);   
+      this.s.timestamp = firebase.firestore.Timestamp.fromDate(new Date(this.s.timestamp) ); 
+      this.modalController.dismiss(this.s);
+    }
   }
-
   cancel(){
     this.modalController.dismiss();
+    
   }
-
+  
   pinLat(value){
     this.lat = value;
-    console.log("lat",value,this.lat);
   }
-
+  
   pinLng(value){
     this.lng = value;
-    console.log("lng",value,this.lng);
   }
+  
+  async alertInput() {
+    const alert = await this.alertController.create({
+      header: 'Information do not complete !!!',
+      message: "Please complete the " + " " + this.message ,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  
 }
